10e47d96f11dd92a4ba5e4cc1a636bad9ec9a1aa
lucasitelvino <lucas.itelvino@gmail.com>
2020-12-12 21:46:38 -0300

    Project created

---
+++ src/main/java/com/iftm/coursePDS1springboot/CoursePds1SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.coursePDS1springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePds1SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePds1SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/coursePDS1springboot/CoursePds1SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.coursePDS1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePds1SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
